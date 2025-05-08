dff41072bcebdba37e6cf5b106abfa59dd60cf52
Isabel Sáenz <isasaete@gmail.com>
2025-05-08 23:29:10 +0200

    Add test to getVinyls endpoint

---
+++ src/vinyl/fixturesDto.ts
@@ -0,0 +1,45 @@
+import { VinylDto } from "./types.js";
+
+export const silentShout: VinylDto = {
+  title: "Silent Shout",
+  artist: "The Knife",
+  country: "Sweden",
+  releaseYear: "2006",
+  genre: "Electro",
+  format: "LP",
+  coverImageUrl: "https://example.com/silent-shout.jpg",
+  style: ["Synth-pop", "Darkwave"],
+  purchasedAt: "Amoeba Music, LA",
+  notes:
+    "Este álbum marcó un giro oscuro y experimental en el dúo sueco, alejándose de lo pop. ",
+  owned: true,
+};
+
+export const mentalGroove: VinylDto = {
+  title: "Mental Groove Classics",
+  artist: "Various Artists",
+  country: "Switzerland",
+  releaseYear: "1998",
+  genre: "Techno",
+  format: "2xLP",
+  coverImageUrl: "https://example.com/mental-groove.jpg",
+  style: ["Dub Techno", "Ambient Techno"],
+  purchasedAt: "Mental Groove Records, Ginebra",
+  notes:
+    "Una recopilación de rarezas y joyas techno suizo que muestra la diversidad y profundidad de la escena.",
+  owned: true,
+};
+
+export const mutant: VinylDto = {
+  title: "Mutant",
+  artist: "Arca",
+  country: "Venezuela",
+  releaseYear: "2015",
+  genre: "Experimental Electronic",
+  format: "LP",
+  coverImageUrl: "https://example.com/mutant-arca.jpg",
+  style: ["Avant-Garde", "Deconstructed Club"],
+  notes:
+    "Sus sonidos rotos y atmósferas inestables lo han consolidado como una figura clave del nuevo sonido latino experimental.",
+  owned: true,
+};
