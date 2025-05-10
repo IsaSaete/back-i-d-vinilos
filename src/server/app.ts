81e1db80744a3dd67a8f0c907d6f0c1b55dc8d9a
Isabel Sáenz <isasaete@gmail.com>
2025-05-10 13:47:51 +0200

    Add handleCors middleware

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,12 +4,15 @@ import checkHealthStatus from "./middleware/checkHealthStatus/checkHealthStatus.
 import handleErrors from "./middleware/handleErrors/handleErrors.js";
 import handleEndpointNotFound from "./middleware/handleEndpointNotFound/handleEndpointNotFound.js";
 import vinylRouter from "../vinyl/router/vinylRouter.js";
+import handleCors from "./middleware/handleCors/handleCors.js";
 
 const app = express();
 
 app.disable("x-powered-by");
 
 app.use(morgan("dev"));
+
+app.use(handleCors);
 
 app.get("/", checkHealthStatus);
 
